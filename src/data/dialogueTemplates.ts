96010c2e19d441acd9c6779dc8e3c1d8e1ec1d55
Kyle <haltermann.kyle@gmail.com>
2025-02-01 22:38:32 -0800

    removed missing label for template

--- src/data/dialogueTemplates.ts
+++ src/data/dialogueTemplates.ts
@@ -1,4 +1,3 @@
-import petraDialogue from '../assets/petraDialogue.json';
 import tutorialDialogue from '../assets/tutorialDialogue.json';
 
 export const dialogueFiles = [
@@ -6,11 +5,6 @@ export const dialogueFiles = [
     name: 'Tutorial',
     path: 'tutorial',
     content: tutorialDialogue,
-  },
-  {
-    name: 'Petra Example',
-    path: 'petra',
-    content: petraDialogue,
   },
 ];
 
